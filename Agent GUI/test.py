ae652304d11d893ccf282b2afd1a5270c2e22d69
S0ykot <soykot.ch@gmail.com>
2020-02-16 01:12:13 +0600

    new update

--- Agent GUI/test.py
+++ Agent GUI/test.py
@@ -2,25 +2,15 @@ from tkinter import *
 import random
 import time
 
-def popupmsg(msg):
-    popup = Tk()
-    popup.wm_title("!")
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
-    label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-    B1.pack()
-    popup.mainloop()
 
 
-
-
-status={'loc_A':random.choice(['Clean','Dirty']),
-        'loc_B':random.choice(['Clean','Dirty']),
-        'loc_C':random.choice(['Clean','Dirty']),
-        'loc_D':random.choice(['Clean','Dirty'])
+status1={'loc_A':random.choice([True,False]),
+        'loc_B':random.choice([True,False]),
+        'loc_C':random.choice([True,False]),
+        'loc_D':random.choice([True,False])
         }
 
-status1 = status;
+
 
 
 root = Tk() 
@@ -60,22 +50,22 @@ lblD= Label(D_frame,text="D",font=("Helvetica", 9)).pack()
 
 print('Before=>',status1)
 
-if (status['loc_A']=='Dirty'):
+if ( not status1['loc_A']):
 	img1 = PhotoImage(file="d.png")
 	photoimage1 = img1.subsample(4,4) 
 	lblr1=Label(A_frame,image=photoimage1).pack(side=LEFT)
 
-if (status['loc_B']=='Dirty'):
+if (not status1['loc_B']):
 	img1 = PhotoImage(file="d.png")
 	photoimage2 = img1.subsample(4,4) 
 	lblr2=Label(B_frame,image=photoimage2).pack(side=LEFT)
 
-if (status['loc_C']=='Dirty'):
+if (not status1['loc_C']):
 	img1 = PhotoImage(file="d.png")
 	photoimage3 = img1.subsample(4,4) 
 	lblr3=Label(C_frame,image=photoimage3).pack(side=LEFT)
 
-if (status['loc_D']=='Dirty'):
+if (not status1['loc_D']):
 	img1 = PhotoImage(file="d.png")
 	photoimage4 = img1.subsample(4,4) 
 	lblr4=Label(D_frame,image=photoimage4).pack(side=LEFT)
@@ -87,46 +77,8 @@ photoimage = img.subsample(2,2)
 lbld=Label(random.choice(env),image=photoimage).pack(side=RIGHT)
 
 
-if (status['loc_A']=='Clean' and status['loc_B']=='Clean' and status['loc_C']=='Clean' and status['loc_D']=='Clean'):
-	print('All clean')
-	popupmsg("All clean")
-
-
-while ((status1['loc_A']!='Clean') and (status1['loc_B']!='Clean') and (status1['loc_C']!='Clean') and (status1['loc_D']!='Clean')):
-	if (status1['loc_A']=='Dirty'):
-		# img = PhotoImage(file="r.gif")
-		# photoimage = img.subsample(2,2) 
-		# lbld=Label(A_frame,image=photoimage).pack(side=RIGHT)
-		status1['loc_A']=='Clean'
-		print('A = >',status1['loc_A'])
-
-
-
-	if (status1['loc_B']=='Dirty'):
-		img = PhotoImage(file="r.gif")
-		photoimage = img.subsample(2,2) 
-		lbld=Label(A_frame,image=photoimage).pack(side=RIGHT)
-		status1['loc_B']=='Clean'
-		print('B = >',status1['loc_B'])
-
-
-	if (status1['loc_C']=='Dirty'):
-		img = PhotoImage(file="r.gif")
-		photoimage = img.subsample(2,2) 
-		lbld=Label(A_frame,image=photoimage).pack(side=RIGHT)
-		status1['loc_C']=='Clean'
-		print('C = >',status1['loc_C'])
-
-
-	if (status1['loc_D']=='Dirty'):
-		img = PhotoImage(file="r.gif")
-		photoimage = img.subsample(2,2) 
-		lbld=Label(A_frame,image=photoimage).pack(side=RIGHT)
-		status1['loc_D']=='Clean'
-		print('D = >',status1['loc_D'])
-
-
-
+if (status1['loc_A'] and status1['loc_B'] and status1['loc_C'] and status1['loc_D']):
+		print('All clean')
 
 
 print('After =>',status1)
